54e34664aa223893888400fc0323d35502544cd5
SodabaOloumi <sodaba.ulomi@gmail.com>
2022-01-10 10:24:36 -0800

    first commit

---
+++ vapp/src/views/Projects.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <form @click="getProject()" >
+            <div>
+              <label>Customer Address:</label>
+              <input type="text" v-model="customerAddress" id="customerAddress" required placeholder="Enter your Address.. ">
+              
+            </div>
+         
+          <button type="submit" class="btn btn-dark">Get</button>
+      </form> 
+    <div class="card" style="width:80%; margin-left:10% ; padding-top: 3%" >
+    <div class="card-body" >
+      <h3 class="card-title" style="font-family: cursive;">Record details</h3>
+      <p class="card-text" > His/Her Full Name :   {{this.project[0]}}</p>
+      <p class="card-text">His/Her Doctor Address :   {{this.project[1]}}</p>
+      <p class="card-text">His/Her patient Address :  {{this.project[2]}}</p>
+      <p class="card-text">Chief Complient :  {{this.project[3]}}</p>
+      <router-link to="/ProjectEdit"></router-link>
+      
+    
+  </div>
+  </div>
+  </div>
+</template>
+
+<script>
+import App from '../index';
+export default {
+ 
+  data() {
+    return {
+      customerAddress : null,
+      project: []
+    };
+  },
+  methods: {
+    // async onDestroy(_customerAddress) {
+    //   const sure = window.confirm('Are you sure?');
+    //   if (!sure) return;
+    //   await App.App.deleteProject(_customerAddress);
+    //   this.flash('task deleted sucessfully!', 'success');
+    //   const newtasks = this.tasks.filter(task => task.customerAddress !== customerAddress);
+    //   this.tasks = newtasks;
+    // },
+    async getProject() {
+        let p = {
+          customerAddress: this.customerAddress,
+           
+      };
+      console.log(this.customerAddress);
+    this.project = await App.App.getProject(p.customerAddress);
+    console.log(this.project);
+    this.customerAddress=null
+  }
+  },
+  
+};
+</script>
